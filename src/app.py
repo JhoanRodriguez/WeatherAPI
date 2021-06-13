ffebe26408c79bce0f268fa3128f6a347e30eaa1
Jhoan Rodriguez <jhoansebastian@msn.com>
2021-06-13 16:03:26 -0500

    Handle cache and adding the requirements.txt

--- src/app.py
+++ src/app.py
@@ -1,11 +1,17 @@
 from flask import Flask, request, jsonify
+from flask_caching import Cache
 from utilities.query_to_api import QueryToApi
 from utilities.format_data import formatData
 
+
+cache = Cache(config={'CACHE_TYPE': 'SimpleCache'})
+
 app = Flask(__name__)
+cache.init_app(app)
 
 
 @app.route('/weather', methods=['GET'])
+@cache.memoize(timeout=120)
 def get_weather():
     city = request.args.get('city', default='Bogota', type=str)
     country = request.args.get('country', default='CO', type=str)
@@ -13,6 +19,7 @@ def get_weather():
     dataFromApi = QueryToApi(city, country)
     finalData = formatData(dataFromApi)
     response.append(finalData)
+    print('se ejecuto')
     return jsonify({'response_items': response}), 200
 
 
